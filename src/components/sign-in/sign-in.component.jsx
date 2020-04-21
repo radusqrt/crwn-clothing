bbc38603f6015ca9c2f49c358741ee74140529ff
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2020-04-21 16:52:12 +0300

    Add redux & auth

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -64,7 +64,11 @@ export default class SignIn extends Component {
               Sign in
             </CustomButton>
 
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type="button"
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+            >
               {" "}
               Sign in with Google{" "}
             </CustomButton>
