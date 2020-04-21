f49c61b4aa84d8ea7d74da5032e0b4517d7e9ed4
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2020-04-21 15:39:38 +0300

    Finished sign in and sign up

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import "./sign-in.styles.scss";
 
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 export default class SignIn extends Component {
   constructor(props) {
@@ -16,10 +16,17 @@ export default class SignIn extends Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   handleChange = (event) => {
@@ -53,7 +60,9 @@ export default class SignIn extends Component {
           />
 
           <div className="buttons">
-            <CustomButton type="submit">Sign in</CustomButton>
+            <CustomButton type="submit" onSubmit={this.handleSubmit}>
+              Sign in
+            </CustomButton>
 
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
               {" "}
